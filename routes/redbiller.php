8fc023ea30f298ce18387628e421fc81f3a198be
Somtoohack <akosasomtoo100@gmail.com>
2025-03-04 14:15:11 +0100

    `Refactor Redbiller webhook routes: replace inspire post route with get route`

--- routes/redbiller.php
+++ routes/redbiller.php
@@ -1,11 +1,15 @@
 <?php
 use App\Http\Controllers\User\WebhookController;
+use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('redbiller')->group(function () {
     Route::prefix('webhook')->group(function () {
 
-        Route::post('inspire', [WebhookController::class, 'inspire'])->name('user');
+        Route::get('inspire', function () {
+            return response()->json(Inspiring::quote(), 200);
+
+        });
         Route::post('/deposit/receive', [WebhookController::class, 'handleDepositWebhook']);
         Route::post('/deposit/verify', [WebhookController::class, 'verifyDepositWebhook']);
 
