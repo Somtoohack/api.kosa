86e66658797414838e0c2dabea53418b28c94473
Somtoohack <akosasomtoo100@gmail.com>
2025-03-17 07:33:54 +0100

    Remove commented-out user factory code and currency seeder call from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,14 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // $this->call(CurrencySeeder::class);
         $this->call(TransactionChargeConfigSeeder::class);
     }
 }
